2058498421168c9730b3aaf4c19c8ec1c61fba00
Alexander Danilov <Tyuiu.DanilovAS@outlook.com>
2025-09-11 12:41:59 +0500

    Task1

---
+++ Tyuiu.DanilovAS.Sprint1.Task1.V13.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.DanilovAS.Sprint1.Task1.V13.Lib;
+namespace Tyuiu.DanilovAS.Sprint1.Task1.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds=  new DataService();
+            double x = 4;
+            double wait = 2;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint1.Task1.V13/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.DanilovAS.Sprint1.Task1.V13.Lib;
+namespace Tyuiu.DanilovAS.Sprint1.Task1.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Исходные данные - x");
+            double x;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.Write("Введите значение X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Ответ равен = {ds.Calculate(x)}");
+            Console.ReadKey();
+        }
+    }
+}
