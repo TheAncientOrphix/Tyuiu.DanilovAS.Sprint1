8f73f575847db9062a9628d760e186b26bf93f84
Alexander Danilov <Tyuiu.DanilovAS@outlook.com>
2025-09-15 23:16:20 +0500

    Task5

---
+++ Tyuiu.DanilovAS.Sprint1.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using Tyuiu.DanilovAS.Sprint1.Task5.V5.Lib;
+namespace Tyuiu.DanilovAS.Sprint1.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculate()
+        {
+            double x = 32.597;
+            DataService ds = new DataService();
+            var wait = 5;
+            var result = ds.Calculate(x);
+            Assert.AreEqual(result, wait);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.DanilovAS.Sprint1.Task5.V5.Lib;
+namespace Tyuiu.DanilovAS.Sprint1.Task5.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+            Console.WriteLine("* Присвоить целой переменной d первую цифру из дробной части              *");
+            Console.WriteLine("* положительного вещественного числа x (так, если x=32.597, то d=5)       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите вещественное число => ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.Calculate(x);
+            Console.WriteLine($"Первая цифра дробной части => {result} ");
+            Console.ReadKey();
+        }
+    }
+}
